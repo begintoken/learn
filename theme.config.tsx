2e381d36173f0a324e1b7b73890c482127fdce04
i8g9 <imazaryag@gmail.com>
2024-04-14 16:29:37 +0700

    [STYLE] : Fixed styling and added glossary page

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+
+const useNextSeoProps = () => {
+  const { asPath } = useRouter()
+  if (asPath !== '/') {
+    return {
+      titleTemplate: 'Begin Token'
+    }
+  }
+}
 
 const config: DocsThemeConfig = {
-  logo: <span>Begin Token</span>,
+  // logo: <span>Begin Token</span>,
+  logo: (
+    <>
+      <span>
+        Begin Token 
+      </span>
+    </>
+  ),
   project: {
     link: 'https://github.com/begintoken/learn',
-  },
-  chat: {
-    link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/begintoken/learn',
   footer: {
     text: '© Begin Token Project 2024',
   },
+  useNextSeoProps,
+  search: {
+    placeholder: 'Search the Knowledge Hub'
+  },
+  sidebar: {
+    toggleButton: true
+  },
+  primaryHue: 116,
 }
 
 export default config
